0215bb93a41aa645fa7af41530d3c9a5b4cd4224
Arno Joosen <arno.joosen@outlook.com>
2025-02-03 18:00:17 +0100

    clean up the parser and help in image builder python script

--- build_image.py
+++ build_image.py
@@ -21,7 +21,7 @@ class DockerfileGenerator:
                 key = key.replace('Dockerfile.', '')
                 template_files[key] = os.path.join(base_templates_path, filename)
         return template_files
-    
+
     def load_ros_templates_options(self):
         template_files = {}
         for filename in os.listdir(ros_templates_path):
@@ -30,7 +30,7 @@ class DockerfileGenerator:
                 key = key.replace('Dockerfile.', '')
                 template_files[key] = os.path.join(ros_templates_path, filename)
         return template_files
-    
+
     def load_entrypoints_templates_options(self):
         template_files = {}
         for filename in os.listdir(entrypoints_templates_path):
@@ -39,71 +39,61 @@ class DockerfileGenerator:
                 key = key.replace('Dockerfile.', '')
                 template_files[key] = os.path.join(entrypoints_templates_path, filename)
         return template_files
-    
+
     def generate_dockerfile(self, base_template, ros_template, entrypoint_template, output_file):
         ros_template = self.ros_templates[ros_template]
         base_template = self.base_templates[base_template]
         entrypoint_template = self.entrypoints_templates[entrypoint_template]
-        
+
         # Read the ROS install Dockerfile template from its file
         with open(ros_template, 'r') as file:
             ros_dockerfile_template = file.read()
-        
+
         # Read the entrypoint Dockerfile template from its file
         with open(entrypoint_template, 'r') as file:
             entrypoint_template_str = file.read()
-        
+
         # Define the context mapping that will be used to render the base template
         context = {
             'ros_install': ros_dockerfile_template,
             'entrypoint_setup': entrypoint_template_str
         }
-        
+
         # Render the base template with the provided context data
         with open(base_template, 'r') as file:
             base_template_str = file.read()
         env = Environment()
         template_obj = env.from_string(base_template_str)
         rendered_dockerfile = template_obj.render(context)
-        
+
         # Write the rendered content to the specified output file
         with open(output_file, 'w') as f:
             f.write(rendered_dockerfile)
 
 def main():
-    # Create a Dockerfile generator
     generator = DockerfileGenerator()
+    parser = argparse.ArgumentParser(description='Dockerfile Generator')
+    subparsers = parser.add_subparsers(dest='command', help='Command to execute')
 
-    # Setup the main parser with an overview of available template options
-    parser = argparse.ArgumentParser(
-        description='Dockerfile Generator\n'
-                    'Available base templates: ' + ', '.join(generator.base_templates.keys()) + '\n'
-                    'Available ROS templates: ' + ', '.join(generator.ros_templates.keys()) + '\n'
-                    'Available entrypoints templates: ' + ', '.join(generator.entrypoints_templates.keys())
-    )
-    subparsers = parser.add_subparsers(dest='command', required=True, help='Sub-command to execute')
+    # Create parser for "gen" command
+    gen_parser = subparsers.add_parser('gen', help='Generate Dockerfile')
+    gen_parser.add_argument('--base', help=f'Base image to use. Options: {list(generator.base_templates.keys())}')
+    gen_parser.add_argument('--ros', help=f'ROS template to use. Options: {list(generator.ros_templates.keys())}')
+    gen_parser.add_argument('--entrypoint', help=f'Entrypoint template to use. Options: {list(generator.entrypoints_templates.keys())}')
+    # Add more arguments as needed for gen command
 
-    # Subparser for the "gen" command with detailed arguments and choices
-    gen_parser = subparsers.add_parser('gen', help='Generate a Dockerfile using specified templates')
-    gen_parser.add_argument('--base', required=True, choices=list(generator.base_templates.keys()),
-                            help='Base template to use. Options: ' + ', '.join(generator.base_templates.keys()))
-    gen_parser.add_argument('--ros', required=True, choices=list(generator.ros_templates.keys()),
-                            help='ROS variant template to use. Options: ' + ', '.join(generator.ros_templates.keys()))
-    gen_parser.add_argument('--entrypoint', required=True, choices=list(generator.entrypoints_templates.keys()),
-                            help='Entrypoint template to use. Options: ' + ', '.join(generator.entrypoints_templates.keys()))
+    # Create parser for "build" command
+    build_parser = subparsers.add_parser('build', help='Build Docker image')
+    build_parser.add_argument('--tag', help='Tag for the image')
+    # Add more arguments as needed for build command
 
-    # Subparser for the "build" command
-    build_parser = subparsers.add_parser('build', help='Build the Docker image')
-    build_parser.add_argument('--image', required=True, help='Name of the image to build')
-    
     args = parser.parse_args()
-    
+
     if args.command == 'gen':
         # Use the selected templates for generation
         print("Selected Base Template:", args.base)
         print("Selected ROS Template:", args.ros)
         print("Selected Entrypoint Template:", args.entrypoint)
-
         # Generation logic here
         generator.generate_dockerfile(args.base, args.ros, args.entrypoint, 'Dockerfile')
 
@@ -113,7 +103,6 @@ def main():
             os.system(f'docker build -t {args.image} .')
         else:
             print("Error: Dockerfile not found. Please generate the Dockerfile first!")
-
 
 if __name__ == '__main__':
     main()
