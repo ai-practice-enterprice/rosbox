4ba1d4bcdca19942e95e0002be15c0d24785511f
Arno Joosen <arno.joosen@outlook.com>
2025-02-06 20:44:11 +0100

    fix no build arg

--- rosbox/rosbox.py
+++ rosbox/rosbox.py
@@ -122,13 +122,15 @@ class ContainerManager:
             print(f"Error removing container: {str(e)}")
             raise
 
-    def build_image(self, base_template, ros_template, image_name):
+    def build_image(self, base_template, ros_template, image_name, no_build):
         self.interactive_builder.generate_dockerfile(base_template, ros_template, 'rosbox')
-        self.interactive_builder.build_image(image_name)
+        if not no_build:
+            self.interactive_builder.build_image(image_name)
 
-    def build_image_it(self, image_name):
+    def build_image_it(self, image_name, no_build):
         self.interactive_builder.generate_dockerfile(entryPoint = 'rosbox')
-        self.interactive_builder.build_image(image_name)
+        if not no_build:
+            self.interactive_builder.build_image(image_name)
 
 def main():
     # check first if docker is installed
@@ -196,9 +198,9 @@ def main():
     elif args.command == 'remove':
         manager.remove_container(args.name)
     elif args.command == 'build':
-        manager.build_image(args.base, args.ros, args.name)
+        manager.build_image(args.base, args.ros, args.name, args.no_build)
     elif args.command == 'ibuilder':
-        manager.build_image_it(args.name)
+        manager.build_image_it(args.name, args.no_build)
 
 if __name__ == "__main__":
     main()
