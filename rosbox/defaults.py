819a548502855ad26d97b63cb981160c3ab65dfd
Arno Joosen <arno.joosen@outlook.com>
2025-02-22 21:49:36 +0100

    change from extensions to default templates

--- rosbox/defaults.py
+++ rosbox/defaults.py
@@ -1,8 +1,8 @@
 DEFAULT_IMAGES = {
-    "desktop": {"base":"universal", "ros":"ros-desktop", "entrypoint":"rosbox", "image-name":"rosbox-desktop"},
-    "robot-jetracer": {"base":"universal", "ros":"ros-base", "entrypoint":"rosbox", "image-name":"rosbox-robot-jetracer"},
-    "robot-jetank": {"base":"universal", "ros":"ros-base", "entrypoint":"rosbox", "image-name":"rosbox-robot-jetank"},
-    "sim": {"base":"universal", "ros":"ros-simulation", "entrypoint":"rosbox", "image-name":"rosbox-sim"}
+    "desktop": {"base":"universal", "ros":"ros-desktop", "entrypoint":"rosbox", "image-name":"rosbox-desktop", "default":"desktop"},
+    "robot-jetracer": {"base":"universal", "ros":"ros-base", "entrypoint":"rosbox", "image-name":"rosbox-robot-jetracer", "default":"robot-jetracer"},
+    "robot-jetank": {"base":"universal", "ros":"ros-base", "entrypoint":"rosbox", "image-name":"rosbox-robot-jetank", "default":"robot-jetank"},
+    "sim": {"base":"universal", "ros":"ros-simulation", "entrypoint":"rosbox", "image-name":"rosbox-sim", "default":"sim"}
 }
 
 DEFAULT_DOCKERHUB_IMAGES = {
