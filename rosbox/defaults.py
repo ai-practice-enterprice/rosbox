bb8a4d48faaf2781df6d9bc6560834096d735c28
Arno Joosen <arno.joosen@outlook.com>
2025-03-07 16:50:44 +0100

    move from ros jazzy to humble

--- rosbox/defaults.py
+++ rosbox/defaults.py
@@ -1,5 +1,6 @@
 DEFAULT_IMAGES = {
     "desktop": {"base":"universal", "ros":"ros-desktop", "entrypoint":"rosbox", "image-name":"rosbox-desktop", "default":"desktop"},
+    "desktopjazzy": {"base":"universal", "ros":"ros-desktopjazzy", "entrypoint":"rosbox", "image-name":"rosbox-desktopjazzy", "default":"desktopjazzy"},
     "robot-jetracer": {"base":"universal", "ros":"ros-base", "entrypoint":"rosbox", "image-name":"rosbox-robot-jetracer", "default":"robot-jetracer"},
     "robot-jetank": {"base":"universal", "ros":"ros-base", "entrypoint":"rosbox", "image-name":"rosbox-robot-jetank", "default":"robot-jetank"},
     "sim": {"base":"universal", "ros":"ros-simulation", "entrypoint":"rosbox", "image-name":"rosbox-sim", "default":"sim"}
@@ -9,5 +10,6 @@ DEFAULT_DOCKERHUB_IMAGES = {
     "robot-jetracer": "docker.io/sterren642/rosbox:robot-jetracer-latest",
     "robot-jetank": "docker.io/sterren642/rosbox:robot-jetank-latest",
     "sim": "docker.io/sterren642/rosbox:sim-latest",
-    "desktop": "docker.io/sterren642/rosbox:desktop-latest"
+    "desktop": "docker.io/sterren642/rosbox:desktop-latest",
+    "desktopjazzy": "docker.io/sterren642/rosbox:desktopjazzy-latest"
 }
